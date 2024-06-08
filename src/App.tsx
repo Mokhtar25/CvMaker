a3436caf4ea09278a65fd83319c91b6b02f3baa2
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-08 23:07:18 +0200

    adding controlled input fields and types for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Header />
+
+      <Card cardName={"Education"} />
 
       <Footer />
     </>
