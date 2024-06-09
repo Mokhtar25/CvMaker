9b059344b43a0501fecadb518610bb9117ea4a73
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-09 15:16:23 +0200

    adding styling to setion

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Card from "./components/Card";
+import Section from "./components/Section";
 
 function App() {
   return (
     <>
       <Header />
 
-      <Card cardName={"Education"} />
+      <Section text={"Education"}>
+        <Card cardName={"Education"} />
+      </Section>
 
       <Footer />
     </>
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -6,23 +6,29 @@ interface Props {
 }
 
 const Section: FC<Props> = (props: Props) => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
+  const style =
+    "border-2 border-black rounded flex w-[25%] transition-[height] ";
+  const styleBanner = "items-center justify-between  px-8 py-2 ";
   const handelOpen = () => {
     setOpen(!open);
   };
 
   if (open) {
     return (
-      <div className="Border-2 border-black rounded h-24 w-52">
-        <h2>{props.text}</h2>
-        <button onClick={handelOpen}>-</button>
+      <div className={style + " flex-col "}>
+        <div className={styleBanner + " flex "}>
+          <h2>{props.text}</h2>
+          <button onClick={handelOpen}>-</button>
+        </div>
+
         {props.children}
       </div>
     );
   } else {
     return (
-      <div className="Border-2 border-black rounded h-24 w-52">
+      <div className={style + " " + styleBanner}>
         <h2>{props.text}</h2>
         <button onClick={handelOpen}>+</button>
       </div>
