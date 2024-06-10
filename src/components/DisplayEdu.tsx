8f4e7a3903a40f8c129c375af0c629c122d36f22
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-10 17:11:27 +0200

    finshing the grid styling

--- src/components/DisplayEdu.tsx
+++ src/components/DisplayEdu.tsx
@@ -5,10 +5,12 @@ import { Data } from "./Card";
 
 export const DisplayEdu: FC<Data> = ({ uniName, endYear, notes, subject }) => {
   return (
-    <div className="display-card border-2 border-black bg-slate-300 p-10">
-      <h2 className="display-subject">{subject}</h2>
-      <h5 className="display-uniName">{uniName}</h5>
-      <span className="display-endYear">{endYear}</span>
+    <div className="display-card border-2 border-black bg-slate-300 p-10 gap-2">
+      <h2 className="display-subject text-3xl">{subject}</h2>
+      <h5 className="display-uniName text-xl text-gray-600">{uniName}</h5>
+      <span className="display-endYear pl-[70%] text-gray-600 text-xl">
+        {endYear}
+      </span>
       <p className="display-notes">{notes}</p>
     </div>
   );
