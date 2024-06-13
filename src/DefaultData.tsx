25072324e5b3e4261e6cad0d9d464714940b5a56
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-13 15:45:06 +0200

    adding defualt information

--- src/DefaultData.tsx
+++ src/DefaultData.tsx
@@ -13,17 +13,19 @@ export const defaultPersonalInfo: Info = {
 };
 
 export const defaultWorkInfo: WorkInfo = {
-  location: "",
+  location: "New york city",
   company: "Startbucks",
-  startDate: "",
-  finishDate: "",
-  notes: "",
-  jobTitle: "",
+  startDate: "2021",
+  finishDate: "2022",
+  notes:
+    "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
+  jobTitle: "Barista",
 };
 
 export const defaultData: Data = {
   uniName: "Paderborn",
-  subject: "",
-  endYear: "",
-  notes: "",
+  subject: "Computer Sciecne",
+  endYear: "2021",
+  notes:
+    "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.§",
 };
